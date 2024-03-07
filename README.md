419f70cdc1f7fa1475c4592b7f97f0e64c6e47c6
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-03-07 17:01:45 +0530

    added

---
+++ README.md
@@ -0,0 +1,5 @@
+<div className="row">
+                            <div className="col-5">
+
+                            </div>
+                        </div>
---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import userRoutes from "./routes/users.routes.js";
+import addMissionFormRoutes from "./routes/addMissionForm.routes.js";
+import studentsRoutes from "./routes/students.routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", userRoutes);
+app.use("/api/addmission_form", addMissionFormRoutes);
+app.use("/api/students", studentsRoutes);
+const __dirname = path.resolve();
+app.use("/images", express.static(path.join(__dirname + "/images")));
+
+export default app;
